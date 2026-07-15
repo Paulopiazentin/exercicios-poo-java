6c043630c8a61d90ed0d387be9fd2d039d78e276
Paulo Piazentin <Paulopiazentin10@gmail.com>
2026-07-15 18:20:01 -0300

    criação das subs classe
    hoje crie mais algumas classe, as subs classe, na mesma logica do Mamifero.java , criei a Ave.java e Reptil.java

---
+++ ex001/Ave.java
@@ -0,0 +1,17 @@
+package ex001;
+
+public class Ave extends Animal {
+    private String corPena;
+
+    @Override
+    public void locomover() {System.out.println("Voando");}
+
+    @Override
+    public void alimentar() {System.out.println("Comendo frutas");}
+
+    @Override
+    public void emitirSom() {System.out.println("som de ave");}
+    public void fazerNinho() {System.out.println("Fazendo ninho");}
+    public String getCorPena() {return corPena;}
+    public void setCorPena(String corPena) {this.corPena = corPena;}
+}
