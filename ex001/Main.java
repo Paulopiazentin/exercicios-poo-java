f94b2d60ebf96d19615bf8f0882fbd7f2563ce0e
Paulo Piazentin <Paulopiazentin10@gmail.com>
2026-07-16 19:24:12 -0300

    Update Main.java
    enfim concluímos o Main onde será exibida as informações descrita em cada classe.
    
    instanciamos cada classe e pedimos ao Java exibir aquilo que foi sobrescrito.

--- ex001/Main.java
+++ ex001/Main.java
@@ -1,7 +1,53 @@
 package ex001;
 
+
 public class Main {
         public static void main (String[] args) {
-            
+
+            Mamifero m = new Mamifero();
+            Reptil r = new Reptil ();
+            Peixe p = new Peixe();
+            Ave a = new Ave();
+            Cachorro k = new Cachorro();
+            Canguru c = new Canguru();
+            Cobra o = new Cobra();
+            Tartaruga t = new Tartaruga();
+            Goldfish g = new Goldfish();
+            Arara f = new Arara();
+
+            System.out.println("\nCachorro");
+            k.emitirSom();
+            k.locomover();
+            k.alimentar();
+
+
+            System.out.println("\nCanguru");
+            c.emitirSom();
+            c.locomover();
+            c.alimentar();
+
+            System.out.println("\nCobra");
+            o.emitirSom();
+            o.locomover();
+            o.alimentar();
+
+            System.out.println("\nTartaruga");
+            t.emitirSom();
+            t.locomover();
+            t.alimentar();
+
+            System.out.println("\nGoldfish");
+            g.emitirSom();
+            g.locomover();
+            g.alimentar();
+            g.soltarBolha();
+
+            System.out.println("\nArara");
+            f.emitirSom();
+            f.locomover();
+            f.alimentar();
+            f.fazerNinho();
+
+
         }
 }
